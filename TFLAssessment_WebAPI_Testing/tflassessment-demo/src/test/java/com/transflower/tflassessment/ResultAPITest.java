0621ac8f3af317d569d1291eea14ac97c0737caf
Sahil161967 <sahilbajkamble@gmail.com>
2025-11-27 14:29:56 +0530

    Added All candidate Score

--- TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/ResultAPITest.java
+++ TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/ResultAPITest.java
@@ -18,6 +18,8 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/subjects ")
     //     .then()
+    //     .body("[0].id",equalTo(1))
+    //     .body("[0].title",equalTo("COREJAVA"))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -74,6 +76,11 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/candidates/1/tests/1/details")
     //     .then()
+    //     .body("candidateId",equalTo(1))
+    //     .body("testId",equalTo(1))
+    //     .body("correctAnswers",equalTo(3))
+    //     .body("incorrectAnswers",equalTo(6))
+    //     .body("skippedQuestions",equalTo(0))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -85,6 +92,13 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/tests/1/detail")
     //     .then()
+    //     .body("[0].testId",equalTo(1))
+    //     .body("[0].testName",equalTo("Basic MCQ Test"))
+    //     .body("[0].candidateId",equalTo(2))
+    //     .body("[0].firstName",equalTo("kajal"))
+    //     .body("[0].lastName",equalTo("ghule"))
+    //     .body("[0].subject",equalTo("COREJAVA"))
+    //     .body("[0].score",equalTo(28))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -96,6 +110,10 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/candidates/tests/1")
     //     .then()
+    //     .body("[0].testId",equalTo(1))
+    //     .body("[0].candidateId",equalTo(2))
+    //     .body("[0].firstName",equalTo("kajal"))
+    //     .body("[0].lastName",equalTo("ghule"))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -107,6 +125,12 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/passedcandidates/tests/1")
     //     .then()
+    //     .body("[0].testId",equalTo(1))
+    //     .body("[0].candidateId",equalTo(2))
+    //     .body("[0].firstName",equalTo("kajal"))
+    //     .body("[0].lastName",equalTo("ghule"))
+    //     .body("[0].passingLevel",equalTo(5))
+    //     .body("[0].score",equalTo(28))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -118,6 +142,12 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/failedcandidates/tests/2")
     //     .then()
+    //     .body("[0].testId",equalTo(2))
+    //     .body("[0].candidateId",equalTo(6))
+    //     .body("[0].firstName",equalTo("sumit"))
+    //     .body("[0].lastName",equalTo("bhor"))
+    //     .body("[0].passingLevel",equalTo(7))
+    //     .body("[0].score",equalTo(1))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -139,6 +169,9 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/testlist/2")
     //     .then()
+    //     .body("[0].testId",equalTo(1))
+    //     .body("[0].testName",equalTo("Basic MCQ Test"))
+    //     .body("[0].score",equalTo(28))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
@@ -150,25 +183,54 @@ public class ResultAPITest {
     //     .when()
     //      .get("/api/Result/results/subjectresults/3")
     //     .then()
+    //     .body("[0].testId",equalTo(5))
+    //     .body("[0].subjectId",equalTo(3))
+    //     .body("[0].candidateId",equalTo(8))
+    //     .body("[0].firstName",equalTo("pranita"))
+    //     .body("[0].lastName",equalTo("mane"))
+    //     .body("[0].subject",equalTo("DOTNET"))
+    //     .body("[0].score",equalTo(10))
     //      .statusCode(200)
     //      .body("size()",greaterThan(0));
     // }
 
-    @Test
-    public void GetTestAverageReport()
+    // @Test
+    // public void GetTestAverageReport()
+    // {
+    //     given()
+    //     .when()
+    //      .get("/api/Result/results/testaveragereport/2")
+    //     .then()
+    //     .body("[0].subjectName",equalTo("COREJAVA"))
+    //     .body("[0].concept",equalTo("OOPS"))
+    //     .body("[0].totalQuestionsAnswered",equalTo(1))
+    //     .body("[0].correctAnswers",equalTo(1))
+    //     .body("[0].percentageCorrect",equalTo(100))
+    //      .statusCode(200)
+    //      .body("size()",greaterThan(0));
+    // }
+
+    //  @Test
+    // public void GetCandidateScore()
+    // {
+    //     given()
+    //     .when()
+    //      .get("/api/Result/candidates/1/tests/1/score")
+    //     .then()
+    //     .body(equalTo("3"))
+    //      .statusCode(200);
+    // }
+
+     @Test
+    public void GetCandidateAllScore()
     {
         given()
         .when()
-         .get("/api/Result/results/testaveragereport/2")
+         .get("/api/Result/candidates/1/scores")
         .then()
-         .statusCode(200)
-         .body("size()",greaterThan(0));
+        .body("[0].testName",equalTo("Basic MCQ Test"))
+        .body("[0].score",equalTo(3))
+         .statusCode(200);
     }
-
-
-
-
-
-
 
 }
