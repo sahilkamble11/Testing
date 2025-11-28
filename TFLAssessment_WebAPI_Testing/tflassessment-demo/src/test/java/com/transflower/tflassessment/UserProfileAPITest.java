80884ac09965c317c91dda0e3379674bdead76a4
Sahil161967 <sahilbajkamble@gmail.com>
2025-11-28 17:43:45 +0530

    Added Update User profile API

--- TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/UserProfileAPITest.java
+++ TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/UserProfileAPITest.java
@@ -11,21 +11,46 @@ public class UserProfileAPITest {
         RestAssured.baseURI="http://localhost:5238";
     }
 
-    @Test
-    public void GetUserProfileById(){
+    // @Test
+    // public void GetUserProfileById(){
+    //     given()
+    //     .when()
+    //     .get("/api/UserProfile/1")
+    //     .then()
+    //     .body("id", equalTo(1))
+    //     .body("aadharId", equalTo("10564789"))
+    //     .body("firstname", equalTo("ravi"))
+    //     .body("lastname", equalTo("tambade"))
+    //     .body("email", equalTo("ravi.tambade@example.com"))
+    //     .body("contactNumber", equalTo("9000000000"))
+    //     .body("password", equalTo(null))
+    //     .body("userRoles.size()", equalTo(0))
+    //     .statusCode(200);
+    // }
+     @Test
+    public void UpdateUserProfile(){
+        String requestBody="""
+                {
+                "id": 1,
+                "aadharId": "10564789",
+                "firstname": "sahil",
+                "lastname": "kamble",
+                "email": "sahil.kamble@example.com",
+                "contactNumber": "7972542628",
+                "password": "12345",
+                "userRoles": []
+                 }
+                """;
         given()
+        .header("Content-Type","application/json")
+        .body(requestBody)
         .when()
-        .get("/api/UserProfile/1")
+        .put("/api/UserProfile/2")
         .then()
-        .body("id", equalTo(1))
-        .body("aadharId", equalTo("10564789"))
-        .body("firstname", equalTo("ravi"))
-        .body("lastname", equalTo("tambade"))
-        .body("email", equalTo("ravi.tambade@example.com"))
-        .body("contactNumber", equalTo("9000000000"))
-        .body("password", equalTo(null))
-        .body("userRoles.size()", equalTo(0))
-        .statusCode(200);
-    }
+        .statusCode(200)
+        .body(equalTo("true"))
+        ;
+}
     
+
 }
