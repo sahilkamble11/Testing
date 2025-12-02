050ba5c01153eb97fb6f3261de628b8a2b2655dc
Sahil161967 <sahilbajkamble@gmail.com>
2025-12-02 18:22:14 +0530

    Fully working User Profile

--- TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/UserProfileAPITest.java
+++ TFLAssessment_WebAPI_Testing/tflassessment-demo/src/test/java/com/transflower/tflassessment/UserProfileAPITest.java
@@ -11,65 +11,65 @@ public class UserProfileAPITest {
         RestAssured.baseURI="http://localhost:5238";
     }
 
-    // @Test
-    // public void GetUserProfileById(){
-    //     given()
-    //     .when()
-    //     .get("/api/UserProfile/1")
-    //     .then()
-    //     .body("id", equalTo(1))
-    //     .body("aadharId", equalTo("10564789"))
-    //     .body("firstname", equalTo("ravi"))
-    //     .body("lastname", equalTo("tambade"))
-    //     .body("email", equalTo("ravi.tambade@example.com"))
-    //     .body("contactNumber", equalTo("9000000000"))
-    //     .body("password", equalTo(null))
-    //     .body("userRoles.size()", equalTo(0))
-    //     .statusCode(200);
-    // }
-//      @Test
-//     public void UpdateUserProfile(){
-//         String requestBody="""
-//                 {
-//                 "id": 1,
-//                 "aadharId": "10564789",
-//                 "firstname": "sahil",
-//                 "lastname": "kamble",
-//                 "email": "sahil.kamble@example.com",
-//                 "contactNumber": "7972542628",
-//                 "password": "12345",
-//                 "userRoles": []
-//                  }
-//                 """;
-//         given()
-//         .header("Content-Type","application/json")
-//         .body(requestBody)
-//         .when()
-//         .put("/api/UserProfile/2")
-//         .then()
-//         .statusCode(200)
-//         .body(equalTo("true"))
-//         ;
-// }
+    @Test
+    public void GetUserProfileById(){
+        given()
+        .when()
+        .get("/api/UserProfile/1")
+        .then()
+        .body("id", equalTo(1))
+        .body("aadharId", equalTo("10564789"))
+        .body("firstname", equalTo("ravi"))
+        .body("lastname", equalTo("tambade"))
+        .body("email", equalTo("ravi.tambade@example.com"))
+        .body("contactNumber", equalTo("9000000000"))
+        .body("password", equalTo(null))
+        .body("userRoles.size()", equalTo(0))
+        .statusCode(200);
+    }
+     @Test
+    public void UpdateUserProfile(){
+        String requestBody="""
+                {
+                "id": 1,
+                "aadharId": "10564789",
+                "firstname": "sahil",
+                "lastname": "kamble",
+                "email": "sahil.kamble@example.com",
+                "contactNumber": "7972542628",
+                "password": "12345",
+                "userRoles": []
+                 }
+                """;
+        given()
+        .header("Content-Type","application/json")
+        .body(requestBody)
+        .when()
+        .put("/api/UserProfile/2")
+        .then()
+        .statusCode(200)
+        .body(equalTo("true"))
+        ;
+}
 
-// @Test
-//     public void GetUserRoleByContactNo(){
-//         given()
-//         .when()
-//         .get("/api/UserProfile/contactno/7972542628")
-//         .then()
-//         .body("id", equalTo(2))
-//         .body("aadharId", equalTo(null))
-//         .body("firstname", equalTo("sahil"))
-//         .body("lastname", equalTo("kamble"))
-//         .body("email", equalTo("kajal.ghule@example.com"))
-//         .body("contactNumber", equalTo("7972542628"))
-//         .body("roles.size()", equalTo(1))
-//         .body("roles[0].id", equalTo(1))
-//         .body("roles[0].name", equalTo("admin"))
-//         .body("roles[0].lob", equalTo(null))
-//         .statusCode(200);
-//     }
+@Test
+    public void GetUserRoleByContactNo(){
+        given()
+        .when()
+        .get("/api/UserProfile/contactno/7972542628")
+        .then()
+        .body("id", equalTo(2))
+        .body("aadharId", equalTo(null))
+        .body("firstname", equalTo("sahil"))
+        .body("lastname", equalTo("kamble"))
+        .body("email", equalTo("kajal.ghule@example.com"))
+        .body("contactNumber", equalTo("7972542628"))
+        .body("roles.size()", equalTo(1))
+        .body("roles[0].id", equalTo(1))
+        .body("roles[0].name", equalTo("admin"))
+        .body("roles[0].lob", equalTo(null))
+        .statusCode(200);
+    }
 
     @Test
     public void GetAllSmeDetails(){
